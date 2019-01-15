69408697a6d71aad04d090c0f8e016dbf4de84af
Volodymyr Silitskyi <volodymyr_silitskyi@epam.com>
2019-01-15 15:07:48 +0200

    Added logger

---
+++ src/test/java/props/EnvironmentPropertyLoader.java
@@ -0,0 +1,18 @@
+package props;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by : Volodymyr_Silitskyi
+ * Created at : 1/11/2019
+ */
+
+
+public class EnvironmentPropertyLoader {
+//    public static String getProperty(final String propertyName){
+//
+//    }
+
+    private static Logger log = LoggerFactory.getLogger(EnvironmentPropertyLoader.class);
+}
